66fd6eb1240067cd13ec1f78e7331897b1b90466
Johan Wentholt <JTM.Wentholt@student.han.nl>
2016-03-15 14:03:31 +0100

    Merge met Kaart + refactor.

--- src/nl/han/ica/ap/boerenbridge/spel/Main.java
+++ src/nl/han/ica/ap/boerenbridge/spel/Main.java
@@ -1,9 +1,9 @@
-package nl.han.ica.ap.boerenbridge.game;
+package nl.han.ica.ap.boerenbridge.spel;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         System.out.println("Hello Boerenbridge!");
     }
 }
