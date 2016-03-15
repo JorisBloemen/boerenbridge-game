d4b08c2c63154b6a774f1e458d18d22f11e2b17b
JorisBloemen <joris.bloemen123@gmail.com>
2016-03-15 11:47:47 +0100

    Initial Project

---
+++ src/nl/han/ica/ap/boerenbridge/game/Main.java
@@ -0,0 +1,9 @@
+package nl.han.ica.ap.boerenbridge.game;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello Boerenbridge!");
+    }
+}
