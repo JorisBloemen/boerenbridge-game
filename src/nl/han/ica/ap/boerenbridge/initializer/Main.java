eaa5774daa2e6eefdac6da4348a832a6a3e43f26
Johan Wentholt <JTM.Wentholt@student.han.nl>
2016-03-29 10:57:53 +0200

    Main verplaatst naar initializer package, spel wordt aangemaakt. (aanmeldingen van spelers moeten nog gebeuren.)

---
+++ src/nl/han/ica/ap/boerenbridge/initializer/Main.java
@@ -0,0 +1,17 @@
+package nl.han.ica.ap.boerenbridge.initializer;
+
+import nl.han.ica.ap.boerenbridge.spel.Spel;
+
+/**
+ * Initialiseert het spel.
+ */
+public class Main {
+
+    /**
+     * Initialiseert het spel.
+     * @param args nvt
+     */
+    public static void main(String[] args) {
+        Spel spel = new Spel();
+    }
+}
