eae9c798579f1c7e0ef7f27bf1bb45142ede9328
Johan Wentholt <JTM.Wentholt@student.han.nl>
2016-03-29 11:34:56 +0200

    Documentatie voor interfaces.

--- src/nl/han/ica/ap/boerenbridge/spel/ISpel.java
+++ src/nl/han/ica/ap/boerenbridge/spel/ISpel.java
@@ -2,7 +2,12 @@ package nl.han.ica.ap.boerenbridge.spel;
 
 /**
  * Interface voor het boerenbridge spel.
+ * Een speler moet zich kunnen aanmelden bij het spel.
  */
 public interface ISpel {
+    /**
+     * Voeg een speler toe aan het spel, start het spel bij voldoende spelers.
+     * @param speler Deelnemer aan het spel.
+     */
     void neemDeel(ISpeler speler);
 }
