7e44dfa13dc3a0f9b79aa15c19e65a7b4739cf8a
Johan Wentholt <JTM.Wentholt@student.han.nl>
2016-03-29 13:37:01 +0200

    Implementatie van speler interface voor een menselijke speler met doc (zonder tests).

--- src/nl/han/ica/ap/boerenbridge/speler/mens/Speler.java
+++ src/nl/han/ica/ap/boerenbridge/speler/mens/Speler.java
@@ -3,6 +3,8 @@ package nl.han.ica.ap.boerenbridge.speler.mens;
 import nl.han.ica.ap.boerenbridge.kaart.Kaart;
 import nl.han.ica.ap.boerenbridge.spel.ISpeler;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringJoiner;
 
@@ -12,6 +14,7 @@ import java.util.StringJoiner;
 public class Speler implements ISpeler {
 
     private String naam;
+    private ArrayList<Kaart> hand;
 
     /**
      * Initialiseert de speler
@@ -19,25 +22,90 @@ public class Speler implements ISpeler {
      */
     public Speler(String naam) {
         this.naam = naam;
+        this.hand = new ArrayList<Kaart>();
+    }
+
+    @Override
+    public String geefNaam() {
+        return this.naam;
     }
 
     @Override
     public int doeBieding() {
-        return 0;
+        printKaarten(this.hand);
+        System.out.print("Doe een bod: ");
+        return vraagInputInteger();
     }
 
     @Override
     public void ontvangKaart(Kaart kaart) {
-
+        this.hand.add(kaart);
+        sorteerKaarten(this.hand);
     }
 
     @Override
     public Kaart geefKaart(ArrayList<Kaart> kaarten) {
-        return null;
+        System.out.println("\nGespeelde kaarten:");
+        printKaarten(kaarten);
+        System.out.println("\nKaarten in hand:");
+        printKaartenMetIndex(this.hand);
+        System.out.print("\nKies een kaart: ");
+        return this.hand.get(vraagInputInteger());
     }
 
     @Override
     public void updateScore(int score) {
+        System.out.println(this.naam + ": " + score);
+    }
 
+    /**
+     *  Soteer kaarten op basis van type en waarde.
+     * @param kaarten De te sorteren set van kaarten.
+     */
+    private void sorteerKaarten(ArrayList<Kaart> kaarten) {
+        kaarten.sort((k1, k2) ->
+                k1.getKaartWaarde().ordinal() - k2.getKaartWaarde().ordinal());
+        kaarten.sort((k1, k2) ->
+                k1.getKaartType().ordinal() - k2.getKaartType().ordinal());
+    }
+
+    /**
+     * Print een lijst met kaarten.
+     * @param kaarten Kaarten die moeten worden geprint.
+     */
+    private void printKaarten(ArrayList<Kaart> kaarten) {
+        for (Kaart kaart : kaarten)
+            System.out.println(kaart.getKaartType().toString() + " " +
+                    kaart.getKaartWaarde().toString());
+    }
+
+    /**
+     * Print een lijst met kaarten geprefixed met de index.
+     * @param kaarten Kaarten die moeten worden geprint.
+     */
+    private void printKaartenMetIndex(ArrayList<Kaart> kaarten) {
+        for (int i = 0; i < kaarten.size(); i++) {
+            System.out.println(i + ") " +
+                    kaarten.get(i).getKaartType().toString() + " " +
+                    kaarten.get(i).getKaartWaarde().toString());
+        }
+    }
+
+    /**
+     * Vraag de speler een integer op te geven.
+     * @return De opgegeven integer.
+     */
+    private int vraagInputInteger() {
+        BufferedReader br = new BufferedReader(
+                new InputStreamReader(System.in));
+
+        String bodstr = null;
+        try {
+            bodstr = br.readLine();
+        } catch (java.io.IOException ioe) {
+            System.err.println("io error: " + ioe.toString());
+        }
+
+        return Integer.parseInt(bodstr);
     }
 }
